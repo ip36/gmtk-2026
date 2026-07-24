6503236c18a7e579438fe548825592362623f87d
Ben <dragonfart7768@gmail.com>
2026-07-23 20:22:06 -0700

    endgoal and tutorial levels

---
+++ chest.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite2D
+var fading
+
+func _ready() -> void:
+	connect("animation_finished", fade)
+	$Area2D.connect("body_entered", collected)
+
+func fade() -> void:
+	fading = true
+
+func collected(who) -> void:
+	if who.is_in_group("player"):
+		play()
+
+func _process(delta: float) -> void:
+	if fading:
+		$ColorRect.color.a += delta
+		if $ColorRect.color.a > 0.9:
+			get_tree().quit()
