cca64de74f64ff69b7786041320c92cdf00f1f3b
Ben <dragonfart7768@gmail.com>
2026-07-25 12:47:54 -0700

    menus

--- keybindbutton.gd
+++ keybindbutton.gd
@@ -1,36 +1,25 @@
 extends Button
 var lookingforkey = false
-var resettimer = 0
-var resettimermax = 1
 var clickdetection
+var origtext
 ## Add the exact name of the inputmap action this button should rebind(Case sensetive.)
 @export var InputToRebind : String = "Example Input"
 
 func _ready():
+	origtext = text
 	connect("button_down", igotpressed)
-
-func _process(delta):
-	if resettimer <= 0 and not lookingforkey:
-		text = "Change keybind: 
-			" + InputToRebind
-	else:
-		resettimer -= delta
 
 func igotpressed():
 	if not lookingforkey:
-		text = "Press the key you want to assign.
-			Click again to cancel"
+		text = "Listening for input.
+			Click to cancel"
 		lookingforkey = true
 	else:
-		text = "Change keybind: 
-			" + InputToRebind
+		text = origtext
 		lookingforkey = false
 
 func _input(event):
 	if event is InputEventKey and event.pressed and lookingforkey:
 		InputMap.action_erase_events(InputToRebind)
 		InputMap.action_add_event(InputToRebind, event)
-		text =  InputToRebind + " set to: 
-			" + str(event.as_text())
-		resettimer = resettimermax
 		lookingforkey = false
