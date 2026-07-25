22669de7d29280ada49278e100eba2eea7443578
ReidCole <reid-cole@outlook.com>
2026-07-25 10:32:58 -0400

    Edit camera follows UI.

---
+++ pickup_resource.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name PickupResource
+
+## Text that will display in the game. E.g. Gravity, Health, Speed, Size
+@export var name: String
+## Which property this pickup will reduce. See player.gd for the list. E.g. speed, jumpspeed, fallspeed, health
+enum PropertyKey {size, speed, jump, gravity, block_size, world_size, health}
+@export var property: PropertyKey
+## Pickup sprite texture. Matches up with name of animations in the AnimatedSprite2D in the propertymover scene.
+@export_enum("red", "green", "blue") var texture: String
