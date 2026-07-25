733846405783171268e07586caaebf6a39b8afa6
ReidCole <reid-cole@outlook.com>
2026-07-25 12:54:53 -0400

    Countdown UI functionality

---
+++ countdown_ui.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var label: Label = $Label
+
+func _process(delta: float) -> void:
+	if !player.current_countdown:
+		label.text = ""
+		return
+
+	label.text = countdown_display_name(player.current_countdown) + ": " + str(
+		player.properties[player.current_countdown]
+	).pad_decimals(1)
+
+func countdown_display_name(countdown: String):
+	if countdown == "size":
+		return "Size"
+	if countdown == "speed":
+		return "Speed"
+	if countdown == "jumpspeed":
+		return "Jump Height"
+	if countdown == "fallspeed":
+		return "Gravity"
+	if countdown == "blocksize":
+		return "Block Size"
+	if countdown == "worldsize":
+		return "World Size"
+	if countdown == "health":
+		return "Health"
+	return countdown.capitalize()
