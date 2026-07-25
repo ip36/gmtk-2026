ed5f36800f7a6772ba82d935cd702a36521ada64
ReidCole <reid-cole@outlook.com>
2026-07-25 12:28:34 -0400

    Ticking down sound

--- player.gd
+++ player.gd
@@ -11,13 +11,17 @@ var current_countdown
 var current_countdown_index
 var reverse_gravity = false
 var changing_blocks = []
-@onready var health_bar: ProgressBar = $ProgressBar
 var colors = [Color(255.014, 0.0, 255.014, 1.0), Color(0.851, 0.753, 0.0, 1.0), Color(0.0, 0.0, 1.0, 1.0), Color(0.0, 1.0, 1.0, 1.0), Color(0.475, 0.235, 0.0, 1.0), Color(0.0, 1.0, 0.0, 1.0), Color(1.0, 0.0, 0.0, 1.0)]
 var currentanim
+var starting_property_values
+@onready var health_bar: ProgressBar = $HealthBar
+@onready var tick_down_sound: AudioStreamPlayer = $TickDownSound
 
 func _ready() -> void:
+	starting_property_values = {}
 	for i in properties:
 		reductions.append(properties[i]/10)
+		starting_property_values[i] = properties[i]
 	for i in get_tree().get_nodes_in_group("changing_blocks"):
 		changing_blocks.append(i)
 	
@@ -27,6 +31,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	currentanim = null
 	if current_countdown:
+		tick_down_sound.pitch_scale = 0.2 + absf((properties[current_countdown] / starting_property_values[current_countdown] / 1.5))
 		properties[current_countdown] -= reductions[current_countdown_index] * delta
 		if current_countdown == "fallspeed" and properties["fallspeed"] < 0:
 			reverse_gravity = true
@@ -72,6 +77,7 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.play(currentanim)
 
 func set_countdown(property) -> void:
+	tick_down_sound.playing = true
 	current_countdown = properties.keys()[property]
 	current_countdown_index = property
 	$AnimatedSprite2D2.modulate = colors[property]
